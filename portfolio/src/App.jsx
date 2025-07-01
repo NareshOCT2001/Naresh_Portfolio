2e58e461590471756ee623409f6c45473a96dbe7
NareshOCT2001 <knareshkumar1910@gmail.com>
2025-07-01 22:03:53 +0530

    Created a React JS Project Using Vite & Installed the Tailwind CSS

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex w-full justify-center'>
+      <h1 class="text-3xl font-bold underline flex justify-center bg-red-950 text-white w-full rounded-3xl p-4 m-auto">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
