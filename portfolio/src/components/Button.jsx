9b30a07c1439d81702977e0b38c94e860c1fd816
NareshOCT2001 <knareshkumar1910@gmail.com>
2025-07-02 10:32:03 +0530

    Altered the design

---
+++ portfolio/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+        <button className='bg-black text-white px-6 py-2 rounded-4xl hover:bg-sky-400 hover:text-blue-950 font-bold'>
+            Get in touch
+        </button>
+    </>
+  )
+}
+
+export default Button
--- portfolio/src/components/HeroSection.jsx
+++ portfolio/src/components/HeroSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Button from './Button';
 function HeroSection() {
   return (
-    <div className='h-dvh pt-40 pl-16 pr-50 bg-amber-300'>
-    <h1 className='text-6xl font-[350] tracking-normal'>Brand-driven product designer,<br /> crafting meaningful identities, systems and experiences.</h1>
-    <h2 className='text-tiny py-10 '>Enthusiastic Web developer ✸  Available for freelance projects</h2>
+    <div className='h-dvh pt-40 pl-16 pr-50 '>
+    <h1 className='text-6xl font-[300] font-inter tracking-normal'>Brand-driven product designer,<br /> crafting meaningful identities, systems and experiences.</h1>
+    <h2 className='text-tiny py-11 '>Enthusiastic Web developer ✸  Available for freelance projects</h2>
+    <Button/>
     </div>
 )
 }
